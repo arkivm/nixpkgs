3427954f891f00eff618ba7ecd10729b1a8b6f6d
Vikram Narayanan <vikram186@gmail.com>
2021-10-09 15:14:38 -0700

    bcompare: add darwin support

--- pkgs/applications/version-management/bcompare/default.nix
+++ pkgs/applications/version-management/bcompare/default.nix
@@ -1,54 +1,26 @@
-{ autoPatchelfHook, bzip2, cairo, coreutils, fetchurl, gdk-pixbuf, pango, gtk2, kcoreaddons, ki18n, kio, kservice, lib, qt4, qtbase, stdenv, runtimeShell }:
+{ autoPatchelfHook, bzip2, cairo, coreutils, fetchurl, gdk-pixbuf, pango, gtk2, kcoreaddons, ki18n, kio, kservice, lib, qt4, qtbase, stdenv, runtimeShell, unzip }:
 
-stdenv.mkDerivation rec {
+let
   pname = "bcompare";
   version = "4.4.0.25886";
 
-  src = fetchurl {
-    url = "https://www.scootersoftware.com/${pname}-${version}_amd64.deb";
-    sha256 = "sha256-zQZrCjXzoOZ5o5M4t1n5/HhGoGTcZSj5rlf9Uz9UZko=";
+  throwSystem = throw "Unsupported system: ${stdenv.hostPlatform.system}";
+
+  srcs = {
+    x86_64-linux = fetchurl {
+      url = "https://www.scootersoftware.com/${pname}-${version}_amd64.deb";
+      sha256 = "sha256-zQZrCjXzoOZ5o5M4t1n5/HhGoGTcZSj5rlf9Uz9UZko=";
+    };
+
+    x86_64-darwin = fetchurl {
+      url = "https://www.scootersoftware.com/BCompareOSX-${version}.zip";
+      sha256 = "sha256-dez30a1sp+4XuBBYhu07Vpn1+AUmX0Ni7aad7hy2ajQ=";
+    };
+
+    aarch64-darwin = srcs.x86_64-darwin;
   };
 
-  unpackPhase = ''
-    ar x $src
-    tar xfz data.tar.gz
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin $out/lib $out/share
-    cp -R usr/share $out/
-    cp -R usr/lib $out/
-    cp -R usr/bin $out/
-
-    # Remove library that refuses to be autoPatchelf'ed
-    rm $out/lib/beyondcompare/ext/bcompare_ext_kde.amd64.so
-
-    substituteInPlace $out/bin/bcompare \
-      --replace "/usr/lib/beyondcompare" "$out/lib/beyondcompare" \
-      --replace "/bin/bash" "${runtimeShell}"
-
-    # Create symlink bzip2 library
-    ln -s ${bzip2.out}/lib/libbz2.so.1 $out/lib/beyondcompare/libbz2.so.1.0
-  '';
-
-  nativeBuildInputs = [ autoPatchelfHook ];
-
-  buildInputs = [
-    stdenv.cc.cc.lib
-    gtk2
-    pango
-    cairo
-    kio
-    kservice
-    ki18n
-    kcoreaddons
-    gdk-pixbuf
-    bzip2
-  ];
-
-  dontBuild = true;
-  dontConfigure = true;
-  dontWrapQtApps = true;
+  src = srcs.${stdenv.hostPlatform.system} or throwSystem;
 
   meta = with lib; {
     description = "GUI application that allows to quickly and easily compare files and folders";
@@ -59,8 +31,66 @@ stdenv.mkDerivation rec {
     '';
     homepage = "https://www.scootersoftware.com";
     license = licenses.unfree;
-    maintainers = [ maintainers.ktor ];
-    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ ktor arkivm ];
+    platforms = builtins.attrNames srcs;
   };
 
-}
+  linux = stdenv.mkDerivation {
+    inherit pname version src meta;
+    unpackPhase = ''
+      ar x $src
+      tar xfz data.tar.gz
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/lib $out/share
+      cp -R usr/share $out/
+      cp -R usr/lib $out/
+      cp -R usr/bin $out/
+
+      # Remove library that refuses to be autoPatchelf'ed
+      rm $out/lib/beyondcompare/ext/bcompare_ext_kde.amd64.so
+
+      substituteInPlace $out/bin/bcompare \
+        --replace "/usr/lib/beyondcompare" "$out/lib/beyondcompare" \
+        --replace "/bin/bash" "${runtimeShell}"
+
+      # Create symlink bzip2 library
+      ln -s ${bzip2.out}/lib/libbz2.so.1 $out/lib/beyondcompare/libbz2.so.1.0
+    '';
+
+    nativeBuildInputs = [ autoPatchelfHook ];
+
+    buildInputs = [
+      stdenv.cc.cc.lib
+      gtk2
+      pango
+      cairo
+      kio
+      kservice
+      ki18n
+      kcoreaddons
+      gdk-pixbuf
+      bzip2
+    ];
+
+    dontBuild = true;
+    dontConfigure = true;
+    dontWrapQtApps = true;
+  };
+
+  darwin = stdenv.mkDerivation {
+    inherit pname version src meta;
+    nativeBuildInputs = [ unzip ];
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      mkdir -p $out/Applications/BCompare.app
+      cp -R . $out/Applications/BCompare.app
+    '';
+  };
+in
+if stdenv.isDarwin
+then darwin
+else linux
